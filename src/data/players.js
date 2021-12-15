525124c7b9593b7431b74cb856ced01fb6b61b86
EvvTim <evgeniytim94@gmail.com>
2021-12-16 00:21:44 +0100

    feat: add player data

---
+++ src/data/players.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    name: 'Jan Kowalski',
+    points: 22,
+    color: 'blue'
+  },
+  {
+    id: 2,
+    name: 'John Doe',
+    points: 33,
+    color: 'green'
+  },
+  {
+    id: 3,
+    name: 'Vasia Pupkin',
+    points: 55,
+    color: 'yellow'
+  }
+];
