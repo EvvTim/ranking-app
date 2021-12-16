5117720b12ac3b934cec3b7a0a547a50157c7c7a
EvvTim <evgeniytim94@gmail.com>
2021-12-16 14:13:28 +0100

    feat: add useTable hook

--- src/Table/useTable.js
+++ src/Table/useTable.js
@@ -1,4 +1,6 @@
 // calculate number of page
+import { useEffect, useState } from 'react';
+
 const calculateRange = (data, rowsPerPage) => {
   const range = [];
   const numberOfPages = Math.ceil(data.length / rowsPerPage);
@@ -13,6 +15,25 @@ const calculateRange = (data, rowsPerPage) => {
 // calculate rows per page
 const sliceData = (data, page, rowsPerPage) => {
   return data.slice((page - 1) * rowsPerPage, page * rowsPerPage);
+};
+
+// hook
+const useTable = (data, page, rowsPerPage) => {
+  const [tableRage, setTableRange] = useState([]);
+  const [tableRows, setTableRows] = useState([]);
+
+  useEffect(() => {
+    const range = calculateRange(data, rowsPerPage);
+    setTableRange([...range]);
+
+    const rows = sliceData(data, page, rowsPerPage);
+    setTableRows([...rows]);
+  }, [data, page, setTableRows, setTableRange]);
+
+  return {
+    tableRows,
+    range: tableRage
+  };
 };
 
 export default useTable;
