4bc6f32f7c4e772e5312a7987104ae7aefe9e358
EvvTim <evgeniytim94@gmail.com>
2021-12-16 14:43:04 +0100

    feat: add Table component

---
+++ src/Table/Table.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import uniqid from 'uniqid';
+import useTable from './useTable';
+import TablePages from './TablePages';
+
+const Table = ({ data, rowsPerPage }) => {
+  const [page, setPage] = useState(1);
+  const { tableRows, range } = useTable(data, page, rowsPerPage);
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Points</th>
+            <th>Color</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableRows.map((el) => (
+            <tr key={uniqid()}>
+              <td>{el.name}</td>
+              <td>{el.points}</td>
+              <td>{el.color}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <TablePages rage={range} tableRows={tableRows} setPage={setPage} page={page} />
+    </>
+  );
+};
+
+export default Table;
