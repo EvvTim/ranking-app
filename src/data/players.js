2eb3bfd4a7125119e6d75ee487fd7042eae48d44
EvvTim <evgeniytim94@gmail.com>
2021-12-16 14:59:59 +0100

    feat: add players data

--- src/data/players.js
+++ src/data/players.js
@@ -16,5 +16,41 @@ export default [
     name: 'Vasia Pupkin',
     points: 55,
     color: 'yellow'
+  },
+  {
+    id: 4,
+    name: 'Piotr Kowalski',
+    points: 90,
+    color: 'red'
+  },
+  {
+    id: 5,
+    name: 'John Smith',
+    points: 27,
+    color: 'purple'
+  },
+  {
+    id: 6,
+    name: 'Ewa Nowak',
+    points: 38,
+    color: 'tomato'
+  },
+  {
+    id: 7,
+    name: 'Paweł',
+    points: 3,
+    color: 'fuchsia'
+  },
+  {
+    id: 8,
+    name: 'xxx',
+    points: 6,
+    color: 'black'
+  },
+  {
+    id: 9,
+    name: 'yyy',
+    points: 0,
+    color: 'azure'
   }
 ];
