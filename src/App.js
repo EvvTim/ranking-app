0f2199e29781483edb6629d8ef773f1163e0b171
EvvTim <evgeniytim94@gmail.com>
2021-12-17 12:23:16 +0100

    feat: add useSearch hook

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import './App.css';
 import React from 'react';
 import Table from './Table/Table';
 import useSort from './hooks/useSort';
+import SearchBar from './SearchBar/SearchBar';
+import useSearch from './hooks/useSearch';
 
 const App = () => {
   const { playersData, sortByPointsHandler, sortByNameHandler, sortByName, sortByPoints } =
     useSort();
+  const { handleSearch, filter } = useSearch(playersData);
+  console.log('filter', filter);
 
   return (
     <div className="app">
+      <SearchBar handleChange={handleSearch} />
       <Table
-        data={playersData}
-        rowsPerPage={5}
-        sortByName={() => sortByNameHandler()}
-        sortByPoints={() => sortByPointsHandler()}
+        data={filter}
+        rowsPerPage={10}
+        sortByName={sortByNameHandler}
+        sortByPoints={sortByPointsHandler}
         pointsCellArrow={sortByPoints ? '↑' : '↓'}
         nameCellArrow={sortByName ? '↑' : '↓'}
       />
