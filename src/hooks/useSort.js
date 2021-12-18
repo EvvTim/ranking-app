22b08dfdd4581921c63f03a27666aa17c3aab20f
EvvTim <evgeniytim94@gmail.com>
2021-12-18 20:39:17 +0100

    refactor: remove data import

--- src/hooks/useSort.js
+++ src/hooks/useSort.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import players from '../data/players';
 
 const useSort = (data) => {
   const [playersData, setPlayersData] = useState([data]);
@@ -16,17 +15,17 @@ const useSort = (data) => {
 
   useEffect(() => {
     if (sortByName) {
-      setPlayersData([...players.sort((a, b) => a.name.localeCompare(b.name))]);
+      setPlayersData([...data.sort((a, b) => a.name.localeCompare(b.name))]);
     } else if (!sortByName) {
-      setPlayersData([...players.sort((a, b) => b.name.localeCompare(a.name))]);
+      setPlayersData([...data.sort((a, b) => b.name.localeCompare(a.name))]);
     }
   }, [sortByName]);
 
   useEffect(() => {
     if (sortByPoints) {
-      setPlayersData([...players.sort((a, b) => a.points - b.points).reverse()]);
+      setPlayersData([...data.sort((a, b) => a.points - b.points).reverse()]);
     } else if (!sortByPoints) {
-      setPlayersData([...players.sort((a, b) => a.points - b.points)]);
+      setPlayersData([...data.sort((a, b) => a.points - b.points)]);
     }
   }, [sortByPoints]);
 
