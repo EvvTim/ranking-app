4ce9f65f28a6687fee97c60dcf13e7c3b0f475bd
EvvTim <evgeniytim94@gmail.com>
2021-12-25 17:31:12 +0100

    fix: fix sort bug

--- src/hooks/useSort.js
+++ src/hooks/useSort.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 
 const useSort = () => {
-  const [sortByName, setSortByName] = useState(false);
-  const [sortByPoints, setSortByPoints] = useState(false);
+  const [sortByName, setSortByName] = useState(true);
+  const [sortByPoints, setSortByPoints] = useState(true);
 
   const sortByPointsHandler = () => {
     setSortByPoints(!sortByPoints);
